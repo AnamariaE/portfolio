fc82b912fcbdde4441f577e3fb892a4a793cc394
AnamariaE <anamariaespinoza@gmail.com>
2026-09-08 16:25:45 +0200

    Temporarily pause public portfolio

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'github-pages-src',
   base: '/portfolio/',
-  publicDir: '../public',
+  publicDir: '../github-pages-hold-public',
   css: { postcss: { plugins: [tailwindcss()] } },
   plugins: [react()],
   build: {
